e53a04fcd6532c2cc5498fdfd38ae2d3abbd3c4f
onesaf <onesaf@onesaf-u1.lan>
2021-02-19 22:17:41 -0500

    I have regained the ability to easily quantize images using OOTB sklearn algorithms and save them. Edge detection and tracing has not been reimplemented yet.

--- PaintByNumbers.py
+++ PaintByNumbers.py
@@ -1,5 +1,5 @@
 """ 
-Grant Lewis Bilker -20201010
+Grant Lewis Bilker -20210219
 
     This project is meant to take any image (preferably higher contrast)
     and turn it into a semi-vectorized template for use as a paint by numbers
@@ -15,74 +15,112 @@ import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.cluster import KMeans
 from sklearn.metrics import pairwise_distances_argmin
-from sklearn.datasets import load_sample_image
 from sklearn.utils import shuffle
 from time import time
-
-n_colors = 64
-
-# Load the Summer Palace photo
-china = load_sample_image("china.jpg")
-
-# Convert to floats instead of the default 8 bits integer coding. Dividing by
-# 255 is important so that plt.imshow behaves works well on float data (need to
-# be in the range [0-1])
-china = np.array(china, dtype=np.float64) / 255
-
-# Load Image and transform to a 2D numpy array.
-w, h, d = original_shape = tuple(china.shape)
-assert d == 3
-image_array = np.reshape(china, (w * h, d))
-
-print("Fitting model on a small sub-sample of the data")
-t0 = time()
-image_array_sample = shuffle(image_array, random_state=0)[:1000]
-kmeans = KMeans(n_clusters=n_colors, random_state=0).fit(image_array_sample)
-print("done in %0.3fs." % (time() - t0))
-
-# Get labels for all points
-print("Predicting color indices on the full image (k-means)")
-t0 = time()
-labels = kmeans.predict(image_array)
-print("done in %0.3fs." % (time() - t0))
+from PIL import Image
 
 
-codebook_random = shuffle(image_array, random_state=0)[:n_colors]
-print("Predicting color indices on the full image (random)")
-t0 = time()
-labels_random = pairwise_distances_argmin(codebook_random,
-                                          image_array,
-                                          axis=0)
-print("done in %0.3fs." % (time() - t0))
+def quantize_image(filename, number_of_colors=24):
+    
+    
+    n_colors = number_of_colors
 
+    # Load the image
+    original_image = Image.open(filename)
 
-def recreate_image(codebook, labels, w, h):
-    """Recreate the (compressed) image from the code book & labels"""
-    d = codebook.shape[1]
-    image = np.zeros((w, h, d))
+    # Convert to floats instead of the default 8-bit integer coding
+    # plt.imshow() requires rgb values [0,1]
+    original_image = np.array(original_image, dtype=np.float64) / 255
+
+    # Transform the image array into a 2D numpy array.
+    w, h, d = original_shape = tuple(original_image.shape)
+    assert d == 3
+    image_array = np.reshape(original_image, (w * h, d))
+    
+    # Grab a shuffled sample of pixels from the image
+    image_array_sample = shuffle(image_array, random_state=0)[:1000]
+    
+    # Fit the pixels and generate cluster centers from the sample data
+    kmeans = KMeans(n_clusters=n_colors, random_state=0).fit(image_array_sample)
+
+    # Get labels for all points
+    # Predict color indices on the full image (k-means)
+    labels = kmeans.predict(image_array)
+    
+    
+    # Recreate the (compressed) image from the cluster centers & labels
+    
+    # Create a "blank" image array
+    quantized_image_array = np.zeros((w, h, d))
+    
     label_idx = 0
+    
     for i in range(w):
         for j in range(h):
-            image[i][j] = codebook[labels[label_idx]]
+            
+            # Fill image with cluster center label index search
+            quantized_image_array[i][j] = kmeans.cluster_centers_[labels[label_idx]]
+            
             label_idx += 1
-    return image
+            
+    return quantized_image_array
 
-# Display all results, alongside original image
-plt.figure(1)
-plt.clf()
-plt.axis('off')
-plt.title('Original image (96,615 colors)')
-plt.imshow(china)
 
-plt.figure(2)
-plt.clf()
-plt.axis('off')
-plt.title('Quantized image (64 colors, K-Means)')
-plt.imshow(recreate_image(kmeans.cluster_centers_, labels, w, h))
 
-plt.figure(3)
-plt.clf()
-plt.axis('off')
-plt.title('Quantized image (64 colors, Random)')
-plt.imshow(recreate_image(codebook_random, labels_random, w, h))
-plt.show()
+def imsave(image_array, filename):
+    
+    my_image = Image.fromarray(np.array(image_array*255, dtype=np.uint8), mode='RGB')
+    my_image.save(filename)
+    
+    pass
+
+
+
+def color_count(countee):
+    
+    if type(countee) == str:
+        
+        # Load the image
+        original_image = Image.open(countee)
+        
+        # Turn image into array
+        original_image = np.array(original_image, dtype=np.float64)
+    
+    else:
+        original_image = countee
+    
+    # Transform the image array into a 2D numpy array.
+    w, h, d = original_shape = tuple(original_image.shape)
+    assert d == 3
+    image_array = np.reshape(original_image, (w * h, d))
+    
+    cc = set()
+    
+    for idx in range(w*h):
+            
+        pixel = tuple(image_array[idx])
+        cc.add(pixel)
+    
+    return len(cc)
+
+
+
+def show(image):
+    
+    plt.figure(1)
+    plt.clf()
+    plt.axis('off')
+    
+    if type(image) == str:
+    
+        imagetype = 'Original'
+        image = Image.open(image)
+        image = np.array(image, dtype=np.float64) / 255
+        
+    else:
+        imagetype = 'Quantized'
+    
+    plt.title(imagetype + ' image w/ ' + str(color_count(image)) + ' colors')
+    plt.imshow(image)
+    
+    pass
